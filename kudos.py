11f204378db3d60003f8bfc4bd8c73e89bd560c4
Terry O'Shea <terrykoshea@gmail.com>
2019-01-23 13:46:48 -0500

    work on receiving/parsing messages

--- kudos.py
+++ kudos.py
@@ -7,7 +7,24 @@ class KudosHandler(object):
         # TODO: fill this in
         return '''[FILL THIS IN]'''
 
+    def parse_message(self, message):
+        # TODO: get sender, receiver, optional message
+        # TODO: if there's receiver, error message
+        return {
+            'sender': 'Terry',
+            'receiver': 'Michelle',
+            'message': 'Great work!',
+            'private': False,
+        }
+
     def handle_message(self, message, bot_handler):
-        bot_handler.send_reply(message, "Thanks for the message!")
+        parsed_message = self.parse_message(message)
+        #bot_handler.send_reply(message, "Thanks for the message!")
+        bot_handler.send_message(dict(
+            type='stream',
+            to='397 Bridge',
+            subject='kudos',
+            content=f"Congratulations to {parsed_message['receiver']}!",
+        ))
 
 handler_class = KudosHandler
