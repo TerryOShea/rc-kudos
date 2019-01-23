473127589b7ef5b55b0676d0039565047aa25fb9
Mihelle Torres <lic.nancy.torres@gmail.com>
2019-01-23 15:46:00 -0500

    We receive messages where bot is mentioned and we mark them as read

--- kudos-server.py
+++ kudos-server.py
@@ -5,13 +5,9 @@ client = zulip.Client(config_file=".zuliprc")
 
 
 
-"""
-@app.route("/kudos")
 def send_kudo():
-    # TODO: get token from coffee chatters
     # TODO: verify against list of users and handles
     # TODO: pull info from parameters: recipient, public/private/anonymous
-    # TODO: These are not public routes 
     request = {
         "type": "stream",
         "to": "kudos",
@@ -20,17 +16,36 @@ def send_kudo():
     }
     result = client.send_message(request)
     print(result)
-"""
 
 def get_messages():
     request = {
         "use_first_unread_anchor": True,
-        "num_before": 4,
+        "num_before": 0,
         "num_after": 8,
         "narrow": [{"operator": "is", "operand": "mentioned"}],
     }
     result = client.get_messages(request)
-    print(result)
+    if result['found_newest']:
+        process_messages(result['messages'])
+
+def process_messages(messages):
+    for message in messages:
+        # TODO Get the format and send_kudo
+        print('The message: ')
+        print(message)
+
+    mark_read([message['id'] for message in messages])
+
+def mark_read(message_ids):
+    if message_ids:
+        request = {
+            'messages': message_ids,
+            'op': 'add',
+            'flag': 'read'
+        }
+        result = client.update_message_flags(request)
+        print('Marked as read? ')
+        print(result)
 
 if __name__ == "__main__":
     while True:
