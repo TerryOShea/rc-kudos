3eb434323cb205ca127baf4370c269337200c978
Terry O'Shea <terrykoshea@gmail.com>
2019-01-24 13:08:33 -0500

    move kudos into a module; see what happens

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
 from http.server import BaseHTTPRequestHandler
+from kudos import Kudos
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
+        k = Kudos()
+        k.get_messages()
         self.end_headers()
         self.wfile.write(str("Hello from Python on Now 2.0!").encode())
         return
